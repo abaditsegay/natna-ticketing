0f7aa0bd3ea22ed192ab6a9c46643c9588ed7723
Meless <meless.weldemichael@gmail.com>
2020-03-30 10:31:24 -0400

    added modal navigation to footers

---
+++ frontend/src/app/purchase-policy/purchase-policy.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-purchase-policy',
+  templateUrl: './purchase-policy.component.html',
+  styleUrls: ['./purchase-policy.component.css']
+})
+export class PurchasePolicyComponent  {
+
+  
+}
